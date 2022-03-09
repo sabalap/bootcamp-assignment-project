a8cf3f45f3572b7c6c7d4c16f8e6c21e026eba78
saba lapanashvili <saba.lapanashvili15@gmail.com>
2022-03-09 15:56:13 +0400

    feat: Add Covid Validation

--- src/components/Form/CovidInformation/CovidInformation.js
+++ src/components/Form/CovidInformation/CovidInformation.js
@@ -1,76 +1,274 @@
+import React, { useState, useEffect, useContext } from "react";
 import classes from "./CovidInformation.module.css";
-import Pagination from "../../Pagination/Pagination";
+import Context from "../../../context/context";
+import previousIcon from "../../../assets/Previous.svg";
+import nextIcon from "../../../assets/Next.svg";
 const CovidInformation = () => {
-  const inputDateTransform = (e) => {
-    e.currentTarget.type = "date";
+  const ctx = useContext(Context);
+  const { changePage } = ctx;
+  const [workPreference, setWorkPreference] = useState("");
+  const [covid, setCovid] = useState("");
+  const [covidDate, setCovidDate] = useState("");
+  const [vaccinated, setVaccinated] = useState("");
+  const [vaccinatedDate, setVaccinatedDate] = useState("");
+
+  const [workPreferenceError, setWorkPreferenceError] = useState("");
+  const [covidError, setCovidError] = useState("");
+  const [covidDateError, setCovidDateError] = useState("");
+  const [vaccinatedError, setVaccinatedError] = useState("");
+  const [vaccinatedDateError, setVaccinatedDateError] = useState("");
+  useEffect(() => {
+    const saveWork = localStorage.getItem("work_preference");
+    const saveCovid = localStorage.getItem("had_covid");
+    const saveCovidDate = localStorage.getItem("had_covid_at");
+    const saveVaccinated = localStorage.getItem("vaccinated");
+    const saveVaccinatedDate = localStorage.getItem("vaccinated_at");
+    if (saveWork) {
+      setWorkPreference(saveWork);
+    }
+    if (saveCovid) {
+      setCovid(JSON.parse(saveCovid));
+    }
+    if (saveCovidDate) {
+      setCovidDate(saveCovidDate);
+    }
+    if (saveVaccinated) {
+      setVaccinated(JSON.parse(saveVaccinated));
+    }
+    if (saveVaccinatedDate) {
+      setVaccinatedDate(saveVaccinatedDate);
+    }
+  }, []);
+  const nextPageHandler = (page) => {
+    setWorkPreference("");
+    setCovidError("");
+    setCovidDateError("");
+    setVaccinatedError("");
+    setVaccinatedDateError("");
+
+    if (page === 4) {
+      if (workPreference === "") {
+        setWorkPreferenceError("* select work preference");
+      } else if (covid === "") {
+        setCovidError("* this field is required");
+      } else if (covid === true && covidDate === "") {
+        setCovidDateError("* select covid date");
+      } else if (vaccinated === "") {
+        setVaccinatedError("* this field is required");
+      } else if (vaccinated === true && vaccinatedDate === "") {
+        setVaccinatedDateError("* select vaccinated date");
+      } else {
+        localStorage.setItem("work_preference", workPreference);
+        localStorage.setItem("had_covid", covid);
+        localStorage.setItem("vaccinated", vaccinated);
+        if (covidDate !== "") {
+          localStorage.setItem("had_covid_at", covidDate);
+        } else {
+          localStorage.removeItem("had_covid_at");
+        }
+        if (vaccinatedDate !== "") {
+          localStorage.setItem("vaccinated_at", vaccinatedDate);
+        } else {
+          localStorage.removeItem("vaccinated_at");
+        }
+        changePage(page);
+      }
+    } else {
+      if (workPreference !== "") {
+        localStorage.setItem("work_preference", workPreference);
+      } else {
+        localStorage.removeItem("work_preference");
+      }
+
+      if (covid !== "") {
+        localStorage.setItem("had_covid", covid);
+      } else {
+        localStorage.removeItem("had_covid");
+      }
+
+      if (vaccinated !== "") {
+        localStorage.setItem("vaccinated", vaccinated);
+      } else {
+        localStorage.removeItem("vaccinated");
+      }
+
+      if (covidDate !== "") {
+        localStorage.setItem("had_covid_at", covidDate);
+      } else {
+        localStorage.removeItem("had_covid_at");
+      }
+
+      if (vaccinatedDate !== "") {
+        localStorage.setItem("vaccinated_at", vaccinatedDate);
+      } else {
+        localStorage.removeItem("vaccinated_at");
+      }
+      changePage(page);
+    }
+  };
+  const covidHandler = (val) => {
+    if (val === "yes") {
+      setCovid(true);
+    } else {
+      setCovid(false);
+      setCovidDate("");
+    }
+  };
+  const vaccinatedHandler = (val) => {
+    if (val === "yes") {
+      setVaccinated(true);
+    } else {
+      setVaccinated(false);
+      setCovidDate("");
+    }
   };
   return (
     <section className={classes["covid-information"]}>
       <h1 className={`question-title ${classes["covid-stuff"]}`}>
         Covid Stuff
       </h1>
-      <form className="form">
+      <div className="form">
         <div className={classes["form-control"]}>
           <div>
             <p className={classes.question}>how would you prefer to work?</p>
+            {workPreference && <p className="error">{workPreferenceError}</p>}
           </div>
-          <div className={classes.options}>
-            <input type="radio" id="from office" value="From Sairme Office" />
-            <label htmlFor="from office">From Sairme Office</label>
-          </div>
-          <div className={classes.options}>
-            <input type="radio" id="from home" value="From Home" />
-            <label htmlFor="from home">From Home</label>
-          </div>
-          <div className={classes.options}>
-            <input type="radio" id="hybrid" value="hybrid" />
-            <label htmlFor="hybrid">Hybrid</label>
-          </div>
+          <form onChange={({ target }) => setWorkPreference(target.value)}>
+            <div className={classes.options}>
+              <input
+                name="work_preference"
+                type="radio"
+                id="office"
+                value="from_office"
+                checked={workPreference === "from_office" ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="office">From Sairme Office</label>
+            </div>
+            <div className={classes.options}>
+              <input
+                name="work_preference"
+                type="radio"
+                id="home"
+                value="from_home"
+                checked={workPreference === "from_home" ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="home">From Home</label>
+            </div>
+            <div className={classes.options}>
+              <input
+                name="work_preference"
+                type="radio"
+                id="hybrid"
+                value="hybrid"
+                checked={workPreference === "hybrid" ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="hybrid">Hybrid</label>
+            </div>
+          </form>
         </div>
         <div className={classes["form-control"]}>
           <p className={classes.question}>Did you contact covid 19? :(</p>
-          <div className={classes.options}>
-            <input type="radio" id="covid19-yes" value="covid19-yes" />
-            <label htmlFor="covid19-yes">Yes</label>
-          </div>
-          <div className={classes.options}>
-            <input type="radio" id="covid19-no" value="covid19-no" />
-            <label htmlFor="covid19-no">No</label>
-          </div>
+          {covidError && <p className="error">{covidError}</p>}
+          <form onChange={({ target }) => covidHandler(target.value)}>
+            <div className={classes.options}>
+              <input
+                name="had_covid"
+                type="radio"
+                id="covid-yes"
+                value="yes"
+                checked={covid === true ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="covid-yes">Yes</label>
+            </div>
+            <div className={classes.options}>
+              <input
+                name="had_covid"
+                type="radio"
+                id="covid-no"
+                value="no"
+                checked={covid === false ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="covid-no">No</label>
+            </div>
+          </form>
         </div>
-        <div className={classes["form-control"]}>
+        <div className={`${covid ? "form-control" : "hidden"}`}>
           <p className={classes.question}>When?</p>
+          {covidDateError && <p className="error">{covidDateError}</p>}
           <input
             className={`field ${classes["date-input"]}`}
-            placeholder="Date"
-            type="text"
-            onFocus={inputDateTransform}
+            placeholder={covidDate !== "" ? covidDate : "Date"}
+            onChange={({ target }) => setCovidDate(target.value)}
+            value={covidDate}
+            type="date"
           />
         </div>
         <div className={classes["form-control"]}>
           <p className={classes.question}>Have you been vaccinated?</p>
-          <div className={classes.options}>
-            <input type="radio" id="vaccinated-yes" value="vaccinated-yes" />
-            <label htmlFor="vaccinated-yes">Yes</label>
-          </div>
-          <div className={classes.options}>
-            <input type="radio" id="vaccinated-no" value="vaccinated-no" />
-            <label htmlFor="vaccinated-no">No</label>
-          </div>
+          {vaccinatedError && <p className="error">{vaccinatedError}</p>}
+          <form onChange={({ target }) => vaccinatedHandler(target.value)}>
+            <div className={classes.options}>
+              <input
+                name="vaccinated"
+                type="radio"
+                id="vaccinated-yes"
+                value="yes"
+                checked={vaccinated === true ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="vaccinated-yes">Yes</label>
+            </div>
+            <div className={classes.options}>
+              <input
+                name="vaccinated"
+                type="radio"
+                id="vaccinated-no"
+                value="no"
+                checked={vaccinated === false ? true : false}
+                onChange={({ target }) => console.log(target.value)}
+              />
+              <label htmlFor="vaccinated-no">No</label>
+            </div>
+          </form>
         </div>
-        <div className={classes["form-control"]}>
+        <div className={`${vaccinated ? "form-control" : "hidden"}`}>
           <p className={classes.question}>
             When did you get your last covid vaccine?
           </p>
+          {vaccinatedDateError && (
+            <p className="error">{vaccinatedDateError}</p>
+          )}
           <input
             className={`field ${classes["date-input"]}`}
-            placeholder="Date"
-            type="text"
-            onFocus={inputDateTransform}
+            placeholder={vaccinatedDate !== "" ? vaccinatedDate : "Date"}
+            type="date"
+            onChange={({ target }) => setVaccinatedDate(target.value)}
+            value={vaccinatedDate}
           />
         </div>
-      </form>
-      <Pagination />
+      </div>
+      <div className="pagination">
+        <img
+          onClick={() => nextPageHandler(2)}
+          src={previousIcon}
+          alt="Previous Arrow"
+        />
+        <span className="activeClass" onClick={() => nextPageHandler(1)}></span>
+        <span className="activeClass" onClick={() => nextPageHandler(2)}></span>
+        <span className="activeClass"></span>
+        <span className="point"></span>
+        <span className="point"></span>
+        <img
+          onClick={() => nextPageHandler(4)}
+          src={nextIcon}
+          alt="Next Arrow"
+        />
+      </div>
     </section>
   );
 };

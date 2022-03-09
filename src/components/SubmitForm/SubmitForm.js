cb0a4781985a232234e4a6d9748e988cb738ed04
saba lapanashvili <saba.lapanashvili15@gmail.com>
2022-03-09 17:21:46 +0400

    feat: Add Submit Functionality

--- src/components/SubmitForm/SubmitForm.js
+++ src/components/SubmitForm/SubmitForm.js
@@ -1,13 +1,22 @@
 import classes from "./SubmitForm.module.css";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import Context from "../../context/context";
 const SubmitForm = () => {
+  const ctx = useContext(Context);
+  const { changePage, handleSubmit, removeUser } = ctx;
+  const prevPageHandler = () => {
+    removeUser();
+    changePage(4);
+  };
   return (
     <section className={classes["submit-section"]}>
       <div className={classes["submit-content"]}>
-        <button className={classes.submit}>Submit</button>
-        <Link to="">
-          <h4 className={classes.back}>go back</h4>
-        </Link>
+        <button className={classes.submit} onClick={handleSubmit}>
+          Submit
+        </button>
+        <h4 className={classes.back} onClick={prevPageHandler}>
+          go back
+        </h4>
       </div>
     </section>
   );
